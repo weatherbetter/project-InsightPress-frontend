44af5ab865100a72aeec036a67412ab3a7d08a7f
weatherbetter12@gmail.com <weatherbetter12@gmail.com>
2023-06-20 20:33:18 +0900

    feature: 메인 화면 구성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.js";
 
 function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element={<div>메인페이지</div>} />
+                <Route path="/" element={<Home></Home>} />
                 <Route path="/article/:id" element={<div>상세페이지</div>} />
                 <Route path="*" element={<div>404</div>} />
             </Routes>
---
+++ src/pages/Home.js
@@ -0,0 +1,78 @@
+function Home() {
+    return (
+        <>
+            <section className="category-section">
+                <div
+                    className="container aos-init aos-animate"
+                    data-aos="fade-up"
+                >
+                    <div className="section-header d-flex justify-content-between align-items-center mb-5">
+                        <h2>Business</h2>
+                        <div>
+                            <a href="category.html" className="more">
+                                See All Business
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="post-entry-1 border-bottom">
+                                <div className="post-meta">
+                                    <span className="date">Business</span>{" "}
+                                    <span className="mx-1">•</span>{" "}
+                                    <span>2023.06.20</span>
+                                </div>
+                                <h2 className="mb-2">
+                                    <a href="single-post.html">
+                                        김기현 “국내 중국인 투표권 제한…건보
+                                        ‘먹튀’도 막겠다”
+                                    </a>
+                                </h2>
+                                <span className="author mb-3 d-block">
+                                    네이버 기사
+                                </span>
+                            </div>
+
+                            <div className="post-entry-1 border-bottom">
+                                <div className="post-meta">
+                                    <span className="date">Business</span>{" "}
+                                    <span className="mx-1">•</span>{" "}
+                                    <span>2023.06.20</span>
+                                </div>
+                                <h2 className="mb-2">
+                                    <a href="single-post.html">
+                                        콜라·참치캔도 가격 올리고 버티기... 기업
+                                        이익은 급증
+                                    </a>
+                                </h2>
+                                <span className="author mb-3 d-block">
+                                    네이버 기사
+                                </span>
+                            </div>
+
+                            <div className="post-entry-1 border-bottom">
+                                <div className="post-meta">
+                                    <span className="date">Business</span>{" "}
+                                    <span className="mx-1">•</span>{" "}
+                                    <span>2023.06.20</span>
+                                </div>
+                                <h2 className="mb-2">
+                                    <a href="single-post.html">
+                                        음악 듣는 사람 뇌 신호 공부한 AI, 히트곡
+                                        97% 맞췄다
+                                    </a>
+                                </h2>
+                                <span className="author mb-3 d-block">
+                                    네이버 기사
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Home;
