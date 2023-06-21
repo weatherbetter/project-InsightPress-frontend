d5e8ccfbd969ea5e6bedeeddc12b89f963160f30
weatherbetter12@gmail.com <weatherbetter12@gmail.com>
2023-06-21 22:46:28 +0900

    feature: 댓글 기능 삭제

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -6,8 +6,6 @@ function Article() {
     const [article, setArticle] = useState({});
     const article_id = useParams().id;
 
-    const [comments, setComments] = useState([]);
-
     useEffect(() => {
         axios
             .get(
@@ -44,108 +42,6 @@ function Article() {
                                 {/* End news content*/}
                             </div>
                             {/* <!-- End Single Post Content --> */}
-
-                            {/* <!-- ======= Comments ======= --> */}
-                            <div className="comments">
-                                <h5 className="comment-title py-4">
-                                    2 Comments
-                                </h5>
-                                <div className="comment d-flex mb-4">
-                                    <div className="flex-grow-1 ms-2 ms-sm-3">
-                                        <div className="comment-meta d-flex align-items-baseline">
-                                            <h6 className="me-2">
-                                                Jordan Singer
-                                            </h6>
-                                            <span className="text-muted">
-                                                2d
-                                            </span>
-                                        </div>
-                                        <div className="comment-body">
-                                            Lorem ipsum, dolor sit amet
-                                            consectetur adipisicing elit. Non
-                                            minima ipsum at amet doloremque qui
-                                            magni, placeat deserunt pariatur
-                                            itaque laudantium impedit aliquam
-                                            eligendi repellendus excepturi
-                                            quibusdam nobis esse accusantium.
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="comment d-flex">
-                                    <div className="flex-shrink-1 ms-2 ms-sm-3">
-                                        <div className="comment-meta d-flex">
-                                            <h6 className="me-2">
-                                                Santiago Roberts
-                                            </h6>
-                                            <span className="text-muted">
-                                                4d
-                                            </span>
-                                        </div>
-                                        <div className="comment-body">
-                                            Lorem ipsum dolor sit amet
-                                            consectetur adipisicing elit. Iusto
-                                            laborum in corrupti dolorum, quas
-                                            delectus nobis porro accusantium
-                                            molestias sequi.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* <!-- End Comments --> */}
-
-                            {/* <!-- ======= Comments Form ======= --> */}
-                            <div className="row justify-content-center mt-5">
-                                <div className="col-lg-12">
-                                    <h5 className="comment-title">
-                                        Leave a Comment
-                                    </h5>
-                                    <div className="row">
-                                        <div className="col-lg-6 mb-3">
-                                            <label htmlFor="comment-name">
-                                                Name
-                                            </label>
-                                            <input
-                                                type="text"
-                                                className="form-control"
-                                                id="comment-name"
-                                                placeholder="Enter your name"
-                                            />
-                                        </div>
-                                        <div className="col-lg-6 mb-3">
-                                            <label htmlFor="comment-email">
-                                                Email
-                                            </label>
-                                            <input
-                                                type="text"
-                                                className="form-control"
-                                                id="comment-email"
-                                                placeholder="Enter your email"
-                                            />
-                                        </div>
-                                        <div className="col-12 mb-3">
-                                            <label htmlFor="comment-message">
-                                                Message
-                                            </label>
-
-                                            <textarea
-                                                className="form-control"
-                                                id="comment-message"
-                                                placeholder="Enter your name"
-                                                cols="30"
-                                                rows="10"
-                                            ></textarea>
-                                        </div>
-                                        <div className="col-12">
-                                            <input
-                                                type="submit"
-                                                className="btn btn-primary"
-                                                value="Post comment"
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* <!-- End Comments Form --> */}
                         </div>
                     </div>
                 </div>
