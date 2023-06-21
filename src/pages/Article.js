a673efd315dd63a796d609c7f38b72dd7e50579a
weatherbetter12@gmail.com <weatherbetter12@gmail.com>
2023-06-22 00:02:33 +0900

    feature: 기사 상세 화면 수정
    - 수정, 삭제 버튼 추가
    - 작성자 구분(일반유저, 관리자)
    - chat gpt alert 색 수정

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -23,18 +23,37 @@ function Article() {
             <section className="single-post-content">
                 <div className="container">
                     <div className="row">
-                        <div
-                            className="col-md-12 post-content"
-                        >
+                        <div className="col-md-12 post-content">
                             {/* <!-- ======= Single Post Content ======= --> */}
                             <div className="single-post">
                                 <div className="post-meta">
                                     <span className="date">Business</span>{" "}
                                     <span className="mx-1">•</span>{" "}
-                                    <span>{article.created_at}</span>
+                                    <span>{article.created_at}</span>{" "}
+                                    <span className="badge text-bg-primary">
+                                        InsightPress
+                                    </span>
                                 </div>
-                                <h1 className="mb-5">{article.title}</h1>
-                                <div className="alert alert-info" role="alert">
+                                <h1 className="">{article.title}</h1>
+                                <span className="mb-3 d-block">
+                                    <button
+                                        type="button"
+                                        className="btn btn-outline-success"
+                                    >
+                                        수정
+                                    </button>{" "}
+                                    |{" "}
+                                    <button
+                                        type="button"
+                                        className="btn btn-outline-danger"
+                                    >
+                                        삭제
+                                    </button>
+                                </span>
+                                <div
+                                    className="alert alert-primary"
+                                    role="alert"
+                                >
                                     {article.gpt_content}
                                 </div>
                                 {/* Start news content*/}
