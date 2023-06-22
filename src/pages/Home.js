116de2cb1093286ad9385fc9ddf69e195a57fe5c
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-22 14:11:20 +0900

    feature: 메인 페이지 제목 위 작은 글씨, 카테고리, 날짜, 사용자 요청 데이터 요청 표시
    - article_request 테이블의 id 와 article 테이블의 request_id와 상호작용
    - request_id: article_request DB로 수집되는 유저 요청한 데이터인지 구분하기 위함
    - category: 뉴스 기사의 분야를 말해줌
    - created_at: 데이터 뉴스 업데이트 일자를 말해줌

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,66 +3,73 @@ import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
 function Home() {
-    const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
+  console.log(`${process.env.REACT_APP_BOARD_API_URL}/articles`);
+  
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BOARD_API_URL}/articles`)
+      .then(response => {
+        setArticles(JSON.parse(response.data.body));
+        console.log(JSON.parse(response.data.body));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get(`${process.env.REACT_APP_BOARD_API_URL}/articles`)
-        .then(response => {
-            setArticles(JSON.parse(response.data.body));
-            console.log(JSON.parse(response.data.body));
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    }, []);
+  console.log(articles);
 
-    return (
-        <>
-            <section className="category-section">
-                <div
-                    className="container aos-init aos-animate"
-                    data-aos="fade-up"
-                >
-                    <div className="section-header d-flex justify-content-between align-items-center mb-5">
-                        <h2>Business</h2>
-                        <div>
-                            <a href="#" className="more">
-                                See All Business
-                            </a>
-                        </div>
-                    </div>
+  return (
+    <>
+      <section className="category-section">
+        <div className="container aos-init aos-animate" data-aos="fade-up">
+          <div className="section-header d-flex justify-content-between align-items-center mb-5">
+            <h2>Business</h2>
+            <div>
+              <a href="#" className="more">
+                See All Business
+              </a>
+            </div>
+          </div>
 
-                    <div className="row">
-                        <div className="col-md-12">
-                            {articles.map((article) => (
-                                <div
-                                    className="post-entry-1 border-bottom"
-                                    key={article.id}
-                                >
-                                    <div className="post-meta">
-                                        <span className="date">
-                                            {article.date}
-                                        </span>
-                                        <span className="mx-1">•</span>
-                                        <span>{article.title}</span>
-                                    </div>
-                                    <h2 className="mb-2">
-                                        <Link to={`/article/${article.id}`}>
-                                            {article.content}
-                                        </Link>
-                                    </h2>
-                                    <span className="author mb-3 d-block">
-                                        네이버 기사
-                                    </span>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
+          <div className="row">
+            <div className="col-md-12">
+              {articles.map((article) => (
+                <div className="post-entry-1 border-bottom" key={article.id}>
+                  <div className="post-meta">
+                    <span className="date">{article.category}</span>
+                    <span className="mx-1">•</span>
+                    <span>{article.created_at}</span>
+					<span className="mx-5" />
+					<span>{article.request_id}</span>
+                  </div>
+                  <h2 className="mb-2">
+                    <Link to={`/article/${article.id}`}>
+                      {article.title}
+                    </Link>
+                  </h2>
+                    <a
+                        href={article.source_url}
+                        className="author mb-3 d-block small"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{
+                            boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.2)",
+                            display: "inline-block",
+                            padding: "4px 8px",
+                        }}
+                    >
+                     original article
+                    </a>
+
                 </div>
-            </section>
-            href
-        </>
-    );
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Home;
