97aeaec13ad749603f5b2cfc134bd3682e7e0b28
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-22 09:30:41 +0900

    feature: axios 필드 관한 하드코딩을 맵 함수로 변경
    - 메인페이지 필드명 axios로 인해 호출된 API 관해서 데이터에서 불러온 해당 뉴스 기사 생성 및 제목 클릭 시 상세 페이지 이동
    - 뉴스 기사 목록의 동적 링크 클릭으로 원문 기사로 이동 가능하게 함

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 
 function Home() {
-    const [ data, setData ] = useState([]);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         axios.get("https://303c43lyua.execute-api.eu-west-1.amazonaws.com/production")
@@ -14,14 +15,10 @@ function Home() {
         });
     }, []);
 
-
     return (
         <>
             <section className="category-section">
-                <div
-                    className="container aos-init aos-animate"
-                    data-aos="fade-up"
-                >
+                <div className="container aos-init aos-animate" data-aos="fade-up">
                     <div className="section-header d-flex justify-content-between align-items-center mb-5">
                         <h2>Business</h2>
                         <div>
@@ -33,60 +30,26 @@ function Home() {
 
                     <div className="row">
                         <div className="col-md-12">
-                            <div className="post-entry-1 border-bottom">
-                                <div className="post-meta">
-                                    <span className="date">Business</span>{" "}
-                                    <span className="mx-1">•</span>{" "}
-                                    <span>2023.06.20</span>
+                            {data.map((d) => (
+                                <div className="post-entry-1 border-bottom" key={d.id}>
+                                    <div className="post-meta">
+                                        <span className="date">{d.field}</span>
+                                        <span className="mx-1">•</span>
+                                        <span>{d.date}</span>
+                                    </div>
+                                    <h2 className="mb-2">
+                                        <Link to={`/article/${d.id}`}>{d.title}</Link>
+                                    </h2>
+                                    <span className="author mb-3 d-block">네이버 기사</span>
                                 </div>
-                                <h2 className="mb-2">
-                                    <a href="/article/1">
-                                        김기현 “국내 중국인 투표권 제한…건보
-                                        ‘먹튀’도 막겠다”
-                                    </a>
-                                </h2>
-                                <span className="author mb-3 d-block">
-                                    네이버 기사
-                                </span>
-                            </div>
-
-                            <div className="post-entry-1 border-bottom">
-                                <div className="post-meta">
-                                    <span className="date">Business</span>{" "}
-                                    <span className="mx-1">•</span>{" "}
-                                    <span>2023.06.20</span>
-                                </div>
-                                <h2 className="mb-2">
-                                    <a href="/article/1">
-                                        콜라·참치캔도 가격 올리고 버티기... 기업
-                                        이익은 급증
-                                    </a>
-                                </h2>
-                                <span className="author mb-3 d-block">
-                                    네이버 기사
-                                </span>
-                            </div>
-
-                            <div className="post-entry-1 border-bottom">
-                                <div className="post-meta">
-                                    <span className="date">Business</span>{" "}
-                                    <span className="mx-1">•</span>{" "}
-                                    <span>2023.06.20</span>
-                                </div>
-                                <h2 className="mb-2">
-                                    <a href="/article/1">
-                                        음악 듣는 사람 뇌 신호 공부한 AI, 히트곡
-                                        97% 맞췄다
-                                    </a>
-                                </h2>
-                                <span className="author mb-3 d-block">
-                                    네이버 기사
-                                </span>
-                            </div>
+                            ))}
+                            
                         </div>
                     </div>
                 </div>
             </section>
+
+            href
         </>
     );
 }
