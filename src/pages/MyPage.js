a9a8af09fa3d22ffa296d6b2ebff044afd8cc0d5
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-23 23:34:30 +0900

    feature: 마이페이지 컴포넌트 스케치
    - db 의 예시 하드코딩 없이 연동이 안되는 상태 **
    - db 와 연결할 수 있는 코드 필요
    - 제대로 작동 못하는 중
    - 람다코드 생성 필요

---
+++ src/pages/MyPage.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { Outlet, Link, Navigate } from 'react-router-dom';
+import axios from 'axios';
+import EditPage from './EditPage.js';
+import Withdraw from './Withdraw.js';
+
+function MyPage() {
+  const [requestQueues, setRequestQueues] = useState([]);
+
+  // Example user information
+  const users = {
+    userid: 'example_userid',
+    username: 'John Doe',
+    email: 'johndoe@example.com',
+  };
+
+  const handleEdit = () => {
+    // Navigate to the edit page
+    // Use the 'Navigate' component from react-router-dom
+    return <Navigate to="/EditPage" />;
+  };
+
+  const handleWithdraw = () => {
+    // Handle withdraw action
+    // This can be a navigation or any other logic you want to perform
+    console.log('Withdraw action triggered');
+  };
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BOARD_API_URL}/mypage`)
+      .then((response) => {
+        setRequestQueues(JSON.parse(response.data.body));
+      })
+      .catch((error) => {});
+  }, []);
+
+  return (
+    <div>
+      <h2>My Page</h2>
+      <p> Id: {users.userid}</p>
+      <p> username: {users.username}</p>
+      <p> email: {users.email}</p> 
+      <p> password: {users.password} </p>
+      <button onClick={handleEdit}>
+        <Link to="/EditPage">Edit My Page</Link>
+      </button>
+      <button onClick={handleWithdraw}>
+        <Link to="/withdraw">Delete My Account</Link>
+      </button>
+
+      <Outlet />
+    </div>
+  );
+}
+
+export default MyPage;
