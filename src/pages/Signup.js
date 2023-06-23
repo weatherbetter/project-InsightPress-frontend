6de744f525ff8bf956ebe42f829e2a17dbff36f7
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-23 23:31:07 +0900

    refactor: 회원가입 컴포넌트 코드 수정 디비와 연동
    - 코드 수정해서 db 와 연동되고 람다와 연동 잘 됨

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,12 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Signup = () => {
   const [id, setId] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [signupSuccess, setSignupSuccess] = useState(false);
 
   const handleIdChange = (e) => setId(e.target.value);
   const handleUsernameChange = (e) => setUsername(e.target.value);
@@ -13,62 +14,101 @@ const Signup = () => {
   const handlePasswordChange = (e) => setPassword(e.target.value);
 
   const handleSignup = () => {
-    const data = {
-      id,
-      username,
-      email,
-      password,
-    };
+    console.log("ID:", id); 
+    console.log("Username:", username);
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+  //   const data = {
+  //     id,
+  //     username,
+  //     email,
+  //     password,
+  //   };
 
     axios
-      .post(`${process.env.REACT_APP_BOARD_API_URL}/signup`, data)
-      .then((res) => console.log(res))
+      .post(`${process.env.REACT_APP_BOARD_API_URL}/auth/signup`, {
+        userid : id,
+        username : username,
+        email : email,
+        password : password,
+      })
+      .then((res) => {
+        console.log("suc");
+        // setSignupSuccess(true); // Set the signup success state
+        // showInputData(); // Call the function to display input data
+      })
       .catch((err) => console.log(err));
   };
 
+  // const showInputData = () => {
+  //   console.log("ID:", id); 
+  //   console.log("Username:", username);
+  //   console.log("Email:", email);
+  //   console.log("Password:", password);
+  // };
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`${process.env.REACT_APP_BOARD_API_URL}/articles`)
+  //     .then((response) => {
+  //       // Process the response data here if needed
+  //       console.log(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
+
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-      <div style={{ marginBottom: "10px", display: "flex", alignItems: "center" }}>
-        <label htmlFor="id" style={{ marginRight: "10px", width: "80px", textAlign: "right" }}>ID:</label>
-        <input
-          type="text"
-          id="id"
-          value={id}
-          onChange={handleIdChange}
-        />
-      </div>
+      {signupSuccess ? (
+        <p>Sign Up Successfully!</p>
+      ) : (
+        <div>
+          <div style={{ marginBottom: "10px", display: "flex", alignItems: "center" }}>
+            <label htmlFor="id" style={{ marginRight: "10px", width: "80px", textAlign: "right" }}>ID:</label>
+            <input
+              type="text"
+              id="id"
+              value={id}
+              onChange={handleIdChange}
+            />
+          </div>
 
-      <div style={{ marginBottom: "10px", display: "flex", alignItems: "center" }}>
-        <label htmlFor="username" style={{ marginRight: "10px", width: "80px", textAlign: "right" }}>Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={handleUsernameChange}
-        />
-      </div>
+          <div style={{ marginBottom: "10px", display: "flex", alignItems: "center" }}>
+            <label htmlFor="username" style={{ marginRight: "10px", width: "80px", textAlign: "right" }}>Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
 
-      <div style={{ marginBottom: "10px", display: "flex", alignItems: "center" }}>
-        <label htmlFor="email" style={{ marginRight: "10px", width: "80px", textAlign: "right" }}>Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-      </div>
+          <div style={{ marginBottom: "10px", display: "flex", alignItems: "center" }}>
+            <label htmlFor="email" style={{ marginRight: "10px", width: "80px", textAlign: "right" }}>Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
 
-      <div style={{ marginBottom: "10px", display: "flex", alignItems: "center" }}>
-        <label htmlFor="password" style={{ marginRight: "10px", width: "80px", textAlign: "right" }}>Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-      </div>
+          <div style={{ marginBottom: "10px", display: "flex", alignItems: "center" }}>
+            <label htmlFor="password" style={{ marginRight: "10px", width: "80px", textAlign: "right" }}>Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
 
-      <button onClick={handleSignup} style={{ marginTop: "10px" }}> Sign Up </button>
+          <button onClick={handleSignup} style={{ marginTop: "10px" }}> Sign Up </button>
+        </div>
+      )}
     </div>
   );
 };
