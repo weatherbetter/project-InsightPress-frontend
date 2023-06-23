36afa66cf71a49c7e7a57947455aec1f0dd4f40f
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-23 23:29:57 +0900

    refactor / bug : 로그인 컴포넌트 콘솔에 찍히도록 수정 ...
    - 람다 쪽 해결 못한 상태
    - (signup 에서 db 저장된 회원정보와 로그인 api 테스트와 연동이 안되는 상태)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,32 +2,53 @@ import axios from "axios";
 import { useState } from "react";
 
 const Login = () => {
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleChangeId = e => setId(e.target.value);
-    const handleChangePassword = e => setPassword(e.target.value);
-    const handleLogin = e => {
-        const data = {
-            id, 
-            password
-        };
-        axios.post(`${process.env.REACT_APP_BOARD_API_URL}/login`, data)
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
+  const handleChangeId = (e) => setId(e.target.value);
+  const handleChangePassword = (e) => setPassword(e.target.value);
+  const handleLogin = (e) => {
+    const data = {
+      userid: id, // Corrected field name from 'id' to 'userid'
+      password: password,
     };
 
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-            <div>
-                ID: <input type="text" value={id} onChange={handleChangeId} />
-                <br/>
-                PW: <input type="password" value={password} onChange={handleChangePassword} />
-                <br/>
-                <button onClick={handleLogin}>로그인</button>
-            </div>
-        </div>
-    );
+    axios
+      .post(`${process.env.REACT_APP_BOARD_API_URL}/auth/login`, data)
+      .then((res) => {
+        console.log(res);
+        if (res.data.success) {
+          // 로그인 성공 시 처리
+          return true;
+        } else {
+          // 로그인 실패 시 처리
+          document.getElementById("showInputData").innerHTML =
+            "Account Not Found: Try Again!";
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <div>
+        ID: <input type="text" value={id} onChange={handleChangeId} />
+        <br />
+        PW: <input type="password" value={password} onChange={handleChangePassword} />
+        <br />
+        <button onClick={handleLogin}>Login</button>
+      </div>
+      <div id="showInputData"></div>
+    </div>
+  );
 };
 
 export default Login;
