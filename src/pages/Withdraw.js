b6f0d15fd48196202df3ec0e28f7d0b6b718b6ad
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-23 23:35:04 +0900

    feature: 회원탈퇴 컴포넌트 스케치
    - api 없음
    - 람다함수 필요
    - 페이지 오류남

---
+++ src/pages/Withdraw.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const WithdrawPage = ({ onWithdraw }) => {
+  useEffect(() => {
+    const deleteAccount = async () => {
+      try {
+        await axios.delete(`${process.env.REACT_APP_BOARD_API_URL}/articles`);
+        console.log('Account deletion successful');
+        onWithdraw(); // Call the onWithdraw callback to perform additional actions if needed
+      } catch (error) {
+        console.error('Account deletion failed:', error);
+      }
+    };
+
+    if (window.confirm("Are you sure you're leaving?")) {
+      deleteAccount();
+    }
+  }, [onWithdraw]);
+
+  return (
+    <div>
+      <h2>Delete Your Account</h2>
+      <p>Sorry for you leaving. But you can come back anytime.</p>
+      <button onClick={onWithdraw}>DELETE MY ACCOUNT</button>
+    </div>
+  );
+};
+
+export default WithdrawPage;
