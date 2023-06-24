0b01d0737cb2247d271e1cbc220d39bccac50b6f
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-24 19:11:35 +0900

    feature: id 값을 null 로 가져오고 나머지 값 수정 가능하도록 함
    - fetchUserId 함수: /auth/login/mypage/{int:id} 엔드포인트로 GET 요청 보내고 서버에서 반환된 회원의 ID 가져오도록 함
    - await axios.put() 으로 비동기 함수 안에서만 Promise객체의 유효한 결과를 처리
    - ex. 네트워크 요청, 파일 입출력, 데이터베이스 쿼리 등 시간이 오래 걸리는 작업과 같은 비동기 작업
    - 비동기 작업 요청 보낸 후 결과 기다리는 동안 다른 작업 수행하도록 함
    - 네트워크 요청 응답 반환 시간동안 유저인터페이스 차단없이 다른 작업 수행 가능
    - Promise 객체가 비동기 작업 결과를 나타냄
    - Promise객체를 await 와 함께 사용해서 비동기 작업 처리될 때까지 함수 실행 일시 중지, 작업 완료 시 결과 반환
    - await 사용, 작업 완료까지 함수의 실행 멈추고, 완료 이후 결과 반환하여 후속 작업 진행 가능하게 함
    - 장점: 데이터 반환 때까지 코드 실행 가능, 코드 실행 흐름이 동기적으로 유지되어 데이터 처리 및 결과를 다음 작업 활용에 편리
    - 비동기 작업을 동기적 코드 흐름으로 작성 가능, 코드의 가독성과 유지 보수 향상

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,42 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const EditPage = ({ userInfo, onSave, onCancel }) => {
+const defaultUserInfo = {
+  id: '',
+  username: '',
+  email: '',
+  password: ''
+};
+
+const EditPage = ({ userInfo = defaultUserInfo, onSave, onCancel }) => {
+  const [userId, setUserId] = useState(userInfo.id);
   const [username, setUsername] = useState(userInfo.username);
   const [email, setEmail] = useState(userInfo.email);
   const [password, setPassword] = useState(userInfo.password);
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/auth/login/mypage/${userId}`);
+        const userData = response.data;
+        setUsername(userData.username);
+        setEmail(userData.email);
+        setPassword(userData.password);
+      } catch (error) {
+        console.error('Failed to fetch user data:', error);
+      }
+    };
+
+    fetchUser();
+  }, [userId]);
+
   const handleSave = async () => {
     try {
-      const response = await axios.put(`${process.env.REACT_APP_API_URL}/auth/login/mypage/{int:id}`, {
+      const response = await axios.put(`${process.env.REACT_APP_API_URL}/auth/login/mypage/${userId}`, {
         username,
         email,
         password
       });
       console.log('User information updated successfully:', response.data);
-      onSave(); // Call the onSave callback to perform additional actions if needed
+      onSave();
     } catch (error) {
       console.error('User information update failed:', error);
     }
   };
 
   const handleCancel = () => {
-    onCancel(); // Call the onCancel callback to perform additional actions if needed
+    onCancel();
   };
 
   return (
     <div>
       <h2>Edit My Page</h2>
-      <label>Username: </label>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <label>Email:</label>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <label>Password: </label>
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <div>
+        <label>ID:</label>
+        <span>{userId}</span>
+      </div>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
       <button onClick={handleSave}>Save</button>
       <button onClick={handleCancel}>Cancel</button>
     </div>
   );
 };
 
-export default EditPage;
+const MyPage = () => {
+  const handleCancel = () => {
+    // 취소 시 수행할 동작을 정의합니다.
+  };
+
+  return (
+    <div>
+      <EditPage onCancel={handleCancel} />
+    </div>
+  );
+};
+
+export default MyPage;
