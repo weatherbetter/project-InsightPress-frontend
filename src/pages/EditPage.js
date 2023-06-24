d6b526a8f094233f6829bfbdd87499c410c0071a
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-24 12:17:03 +0900

    feature: 회원정보수정페이지 axios.put api 연동 추가

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const EditPage = ({ userInfo, onSave }) => {
   const [username, setUsername] = useState(userInfo.username);
   const [email, setEmail] = useState(userInfo.email);
 
-  const handleSave = () => {
-    onSave({ username, email });
+  const handleSave = async () => {
+    try {
+      const response = await axios.put(`${process.env.REACT_APP_API_URL}/user`, {
+        username,
+        email,
+      });
+      console.log('User information updated successfully:', response.data);
+      onSave(); // Call the onSave callback to perform additional actions if needed
+    } catch (error) {
+      console.error('User information update failed:', error);
+    }
   };
 
   return (
