cbb4f5da604cd9a601785d5d6a144a5ab8fc9a75
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-24 12:21:30 +0900

    feature: 비밀번호변경 console 추가

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const EditPage = ({ userInfo, onSave }) => {
+const EditPage = ({ userInfo, onSave, onCancel }) => {
   const [username, setUsername] = useState(userInfo.username);
   const [email, setEmail] = useState(userInfo.email);
+  const [password, setPassword] = useState(userInfo.password);
 
   const handleSave = async () => {
     try {
-      const response = await axios.put(`${process.env.REACT_APP_API_URL}/user`, {
+      const response = await axios.put(`${process.env.REACT_APP_API_URL}/editpage`, {
         username,
         email,
+        password
       });
       console.log('User information updated successfully:', response.data);
       onSave(); // Call the onSave callback to perform additional actions if needed
@@ -18,14 +20,21 @@ const EditPage = ({ userInfo, onSave }) => {
     }
   };
 
+  const handleCancel = () => {
+    onCancel(); // Call the onCancel callback to perform additional actions if needed
+  };
+
   return (
     <div>
-      <h2>회원 정보 수정</h2>
-      <label>이름:</label>
+      <h2>Edit My Page</h2>
+      <label>Username: </label>
       <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <label>이메일:</label>
+      <label>Email:</label>
       <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <button onClick={handleSave}>저장</button>
+      <label>Password: </label>
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleSave}>Save</button>
+      <button onClick={handleCancel}>Cancel</button>
     </div>
   );
 };
