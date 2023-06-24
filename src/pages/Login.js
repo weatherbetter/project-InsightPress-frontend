019093ee31cf1aa1cb8a281a15ad2e0665ce3c3d
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-24 22:44:10 +0900

    feature: 로그인 엔터버튼 가능, 로그인 성공 alert 메시지 반환

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,6 +10,7 @@ const Login = () => {
   const handleChangeId = (e) => setId(e.target.value);
   const handleChangePassword = (e) => setPassword(e.target.value);
   const handleLogin = (e) => {
+    e.preventDefault(); // Prevent form submission
     const data = {
       userid: id,
       password: password,
@@ -22,12 +23,20 @@ const Login = () => {
         if (res.data.statusCode === 200) {
           sessionStorage.setItem("JWT_TOKEN", res.data.body);
           navigate('/');
+          document.getElementById("showInputData").textContent = `ID: ${id} | PW: ${password}`; // Show input data
+          alert('Welcome to InsightPress!');
         } else {
           sessionStorage.clear();
           alert('No User Found. Please Try Again.');
         }
       })
       .catch((err) => console.log(err));
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleLogin(e);
+    }
   };
 
   return (
@@ -44,7 +53,7 @@ const Login = () => {
         ID: <input type="text" value={id} onChange={handleChangeId} />
         <br />
         PW:{" "}
-        <input type="password" value={password} onChange={handleChangePassword} />
+        <input type="password" value={password} onChange={handleChangePassword} onKeyPress={handleKeyPress} />
         <br />
         <button onClick={handleLogin}>Login</button>
       </div>
