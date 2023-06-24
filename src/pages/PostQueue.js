c12e5c60ed10951e7ce11b536b47017b960a9805
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-24 19:31:46 +0900

    feature: jwt 토큰 로그인 인증 성공한 회원만 페이지 방문 가능하도록 수정
    - isLoggedIn 변수가 Redux 상태로 로그인 여부 나타냄
    - 페이지에서 isLoggedIn 이 false 일 경우 alert 메시지 출력
    
    Closes #5

--- src/pages/PostQueue.js
+++ src/pages/PostQueue.js
@@ -1,129 +1,103 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setUpdatePost } from "../store.js";
-import { useNavigate } from "react-router-dom";
 
 const article_category = {
-    0: "정치",
-    1: "경제",
+  0: "정치",
+  1: "경제",
 };
 
 function PostQueue() {
-    const [requestQueues, setRequestQueues] = useState([]);
-    let dispatch = useDispatch();
-    let navigate = useNavigate();
+  const [requestQueues, setRequestQueues] = useState([]);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
 
-    const handlerDelete = (id) => {
-        axios
-            .delete(
-                `${process.env.REACT_APP_BOARD_API_URL}/articles/customer-requests/${id}`
-            )
-            .then((res) => {
-                requestQueues.splice(
-                    requestQueues.findIndex((item) => item.request_id === id),
-                    1
-                );
-                setRequestQueues([...requestQueues]);
-            })
-            .catch((err) => {});
-    };
+  const handleDelete = (id) => {
+    axios
+      .delete(`${process.env.REACT_APP_BOARD_API_URL}/articles/customer-requests/${id}`)
+      .then((res) => {
+        const updatedRequestQueues = requestQueues.filter((item) => item.request_id !== id);
+        setRequestQueues(updatedRequestQueues);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const handlerUpdate = (resQueue) => {
-        dispatch(setUpdatePost(resQueue));
-        console.log(resQueue);
-        navigate(`/updatepost/${resQueue.request_id}`);
-    };
+  const handleUpdate = (resQueue) => {
+    dispatch(setUpdatePost(resQueue));
+    navigate(`/updatepost/${resQueue.request_id}`);
+  };
 
-    useEffect(() => {
-        axios
-            .get(
-                `${process.env.REACT_APP_BOARD_API_URL}/articles/customer-requests`
-            )
-            .then((response) => {
-                setRequestQueues(response.data.body);
-                console.log(response.data.body);
-            })
-            .catch((error) => {});
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BOARD_API_URL}/articles/customer-requests`)
+      .then((response) => {
+        setRequestQueues(response.data.body);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    return (
-        <>
-            <section className="category-section">
-                <div
-                    className="container aos-init aos-animate"
-                    data-aos="fade-up"
-                >
-                    <div className="section-header d-flex justify-content-between align-items-center mb-5">
-                        <h2>Post Queue</h2>
-                        <div></div>
-                    </div>
+  if (!isLoggedIn) {
+    return <div>Please Login to See This Page.</div>;
+  }
 
-                    <div className="row">
-                        <div className="col-md-12">
-                            {requestQueues.length === 0 && (
-                                <div>데이터가 없습니다</div>
-                            )}
-                            {requestQueues.map((resQueue, index) => {
-                                return (
-                                    <div
-                                        className="post-entry-1 border-bottom"
-                                        key={index}
-                                    >
-                                        <div className="post-meta">
-                                            <span className="date">
-                                                {
-                                                    article_category[
-                                                        resQueue.category
-                                                    ]
-                                                }
-                                            </span>{" "}
-                                            <span className="mx-1">•</span>{" "}
-                                            <span>{resQueue.created_at}</span>{" "}
-                                            <span className="mx-1">•</span>{" "}
-                                            <span>{resQueue.user_id}</span>
-                                        </div>
-                                        <h2 className="mb-2">
-                                            <a
-                                                href={resQueue.source_url}
-                                                target="_blank"
-                                            >
-                                                {resQueue.source_url}
-                                            </a>
-                                        </h2>
-                                        <span className="author mb-3 d-block">
-                                            <button
-                                                onClick={() =>
-                                                    handlerUpdate(resQueue)
-                                                }
-                                                type="button"
-                                                className="btn btn-outline-secondary"
-                                            >
-                                                수정
-                                            </button>{" "}
-                                            |{" "}
-                                            <button
-                                                onClick={() =>
-                                                    handlerDelete(
-                                                        resQueue.request_id
-                                                    )
-                                                }
-                                                type="button"
-                                                className="btn btn-outline-danger"
-                                            >
-                                                삭제
-                                            </button>
-                                        </span>
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </div>
+  return (
+    <>
+      <section className="category-section">
+        <div className="container aos-init aos-animate" data-aos="fade-up">
+          <div className="section-header d-flex justify-content-between align-items-center mb-5">
+            <h2>Post Queue</h2>
+            <div></div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-12">
+              {requestQueues.length === 0 && <div>데이터가 없습니다</div>}
+              {requestQueues.map((resQueue, index) => (
+                <div className="post-entry-1 border-bottom" key={index}>
+                  <div className="post-meta">
+                    <span className="date">{article_category[resQueue.category]}</span>{" "}
+                    <span className="mx-1">•</span>{" "}
+                    <span>{resQueue.created_at}</span>{" "}
+                    <span className="mx-1">•</span>{" "}
+                    <span>{resQueue.user_id}</span>
+                  </div>
+                  <h2 className="mb-2">
+                    <a href={resQueue.source_url} target="_blank">
+                      {resQueue.source_url}
+                    </a>
+                  </h2>
+                  <span className="author mb-3 d-block">
+                    <button
+                      onClick={() => handleUpdate(resQueue)}
+                      type="button"
+                      className="btn btn-outline-secondary"
+                    >
+                      수정
+                    </button>{" "}
+                    |{" "}
+                    <button
+                      onClick={() => handleDelete(resQueue.request_id)}
+                      type="button"
+                      className="btn btn-outline-danger"
+                    >
+                      삭제
+                    </button>
+                  </span>
                 </div>
-            </section>
-        </>
-    );
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default PostQueue;
