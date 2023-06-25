7f80d85efa93772529ab66470ddec0b0fbe2ad45
weatherbetter12@gmail.com <weatherbetter12@gmail.com>
2023-06-25 10:16:11 +0900

    feature: Pagination

--- src/pages/ArticleCategory.js
+++ src/pages/ArticleCategory.js
@@ -2,12 +2,16 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import qs from "qs";
 
 function ArticleCategory() {
+    const pagination = 5; // 페이지 넘버링 수
+    const pagination_before_after = 2; // 페이지 넘버링 앞 뒤 수
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPageCount, setTotalPageCount] = useState(3);
+
     const [searchParams, setSearchParams] = useSearchParams();
-    console.log(searchParams.get("category"));
-    const [articles, setArticles] = useState([
+    const [articles, setArticles] = useState(
+        [
         {
             article_id: "3dbds",
             category: 0,
@@ -24,21 +28,42 @@ function ArticleCategory() {
             source_url:
                 "https://n.news.naver.com/mnews/hotissue/article/277/0005274196?type=series&cid=1089768",
         },
-    ]);
+        {
+            article_id: "3dbds",
+            category: 0,
+            created_at_article: "2023-06-21",
+            title: '[칩톡]GPU 집중 속 "인수"로 몸집 키운 엔비디아',
+            source_url:
+                "https://n.news.naver.com/mnews/hotissue/article/277/0005274196?type=series&cid=1089768",
+        },
+    ]
+    );
     const article_category = useSelector((state) => {
         return state.article_category;
     });
 
-    // useEffect(() => {
-    //     axios
-    //         .get(`${process.env.REACT_APP_BOARD_API_URL}/articles?category=${searchParams.get("category")}`)
-    //         .then((response) => {
-    //            console.log(JSON.parse(response.data.body));
-    //             setArticles(JSON.parse(response.data.body));
-    //         })
-    //         .catch((error) => {});
-    // }, []);
+    useEffect(() => {
+        //     axios
+        //         .get(`${process.env.REACT_APP_BOARD_API_URL}/articles?category=${searchParams.get("category")}`)
+        //         .then((response) => {
+        //             setArticles(JSON.parse(response.data.body.articles));
+        //              setTotalPageCount(
+        //                  response.data.body.total_page_count
+        //              );
+        //         })
+        //         .catch((error) => {});
+    }, []);
 
+    const move_page = (sidePage) => {
+        setCurrentPage(sidePage);
+        //     axios
+        //         .get(`${process.env.REACT_APP_BOARD_API_URL}/articles?category=${searchParams.get("category")}&page=${sidePage}`)
+        //         .then((response) => {
+        //             setArticles(JSON.parse(response.data.body.articles));
+        // setTotalPageCount(response.data.body.total_page_count);
+        //         })
+        //         .catch((error) => {});
+    };
     return (
         <>
             <section className="category-section">
@@ -93,30 +118,89 @@ function ArticleCategory() {
                     </div>
 
                     <nav aria-label="Page navigation example">
-                        <ul class="pagination justify-content-center">
-                            <li class="page-item disabled">
-                                <a class="page-link">Previous</a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">
-                                    1
-                                </a>
-                            </li>
-                            <li class="page-item active" aria-current="page">
-                                <a class="page-link" href="#">
-                                    2
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">
-                                    3
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">
-                                    Next
-                                </a>
-                            </li>
+                        <ul className="pagination justify-content-center">
+                            {currentPage === 1 ? (
+                                <li className="page-item disabled">
+                                    <a className="page-link">Previous</a>
+                                </li>
+                            ) : (
+                                <li className="page-item">
+                                    <a
+                                        className="page-link"
+                                        onClick={() =>
+                                            move_page(currentPage - 1)
+                                        }
+                                    >
+                                        Previous
+                                    </a>
+                                </li>
+                            )}
+
+                            {[...Array(parseInt(pagination))].map(
+                                (n, index) => {
+                                    if (
+                                        currentPage +
+                                            index -
+                                            pagination_before_after ==
+                                        currentPage
+                                    ) {
+                                        return (
+                                            <li
+                                                className="page-item active"
+                                                key={index}
+                                            >
+                                                <a
+                                                    className="page-link"
+                                                    href="#"
+                                                >
+                                                    {currentPage}
+                                                </a>
+                                            </li>
+                                        );
+                                    } else {
+                                        let sidePage =
+                                            currentPage +
+                                            index -
+                                            pagination_before_after;
+                                        if (
+                                            sidePage > 0 &&
+                                            sidePage < totalPageCount + 1
+                                        ) {
+                                            return (
+                                                <li
+                                                    className="page-item"
+                                                    key={index}
+                                                >
+                                                    <a
+                                                        className="page-link"
+                                                        onClick={() =>
+                                                            move_page(sidePage)
+                                                        }
+                                                    >
+                                                        {sidePage}
+                                                    </a>
+                                                </li>
+                                            );
+                                        }
+                                    }
+                                }
+                            )}
+                            {currentPage === totalPageCount ? (
+                                <li className="page-item disabled">
+                                    <a className="page-link">Next</a>
+                                </li>
+                            ) : (
+                                <li className="page-item">
+                                    <a
+                                        className="page-link"
+                                        onClick={() =>
+                                            move_page(currentPage + 1)
+                                        }
+                                    >
+                                        Next
+                                    </a>
+                                </li>
+                            )}
                         </ul>
                     </nav>
                 </div>
