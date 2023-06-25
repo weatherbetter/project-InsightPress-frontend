562a3f420d638e8e7e9f77467ed0f489de72ea87
weatherbetter12@gmail.com <weatherbetter12@gmail.com>
2023-06-25 19:14:05 +0900

    feature: 404 에러

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,74 +1,106 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const EditPage = ({ onSave, onCancel }) => {
-  const [userInfo, setUserInfo] = useState(null);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [userInfo, setUserInfo] = useState(null);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      try {
-        const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/mypage`);
-        const userData = response.data;
-        setUserInfo(userData);
-        setUsername(userData.username);
-        setEmail(userData.email);
-        setPassword(userData.password);
-      } catch (error) {
-        console.error('Failed to fetch user data:', error);
-      }
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const token = sessionStorage.getItem("JWT_TOKEN");
+                if (token) {
+                    const response = await axios.post(
+                        `${process.env.REACT_APP_BOARD_API_URL}/auth/mypage`,
+                        {
+                            token: token,
+                        }
+                    );
+                    console.log(response);
+                    if (response.data.statusCode === 200) {
+                        setUserInfo(response.data.body);
+                        setUsername(response.data.body.username);
+                        setEmail(response.data.body.email);
+                        setPassword(response.data.body.password);
+                    }
+                } else {
+                    setUserInfo(null);
+                }
+            } catch (error) {
+                console.error("Failed to fetch user data:", error);
+                setUserInfo(null);
+            }
+        };
+
+        fetchUserInfo();
+    }, []);
+
+    const handleSave = () => {
+        console.log(`${process.env.REACT_APP_BOARD_API_URL}/auth/editpage`);
+        axios
+            .put(`${process.env.REACT_APP_BOARD_API_URL}/auth/editpage`, {
+                token: localStorage.getItem("token"),
+                username,
+                email,
+                password,
+            })
+            .then((response) => {
+                console.log(
+                    "User information updated successfully:",
+                    response.data
+                );
+                onSave(); // Call the onSave callback to perform additional actions if needed
+                alert("User info updated");
+            })
+            .catch((error) => {
+                console.error("User information update failed:", error);
+            });
     };
 
-    fetchUserInfo();
-  }, []);
+    const navigate = useNavigate();
 
-  const handleSave = async () => {
-    try {
-      const response = await axios.put(`${process.env.REACT_APP_API_URL}/auth/editpage`, {
-        token: localStorage.getItem('token'), // JWT 토큰을 요청에 포함하여 서버로 전송
-        username,
-        email,
-        password
-      });
-      console.log('User information updated successfully:', response.data);
-      onSave(); // Call the onSave callback to perform additional actions if needed
-    } catch (error) {
-      console.error('User information update failed:', error);
-    }
-  };
+    const handleCancel = () => {
+        navigate("/MyPage");
+    };
 
-  const navigate = useNavigate();
-
-  const handleCancel = () => {
-    navigate('/MyPage');
-  };
-
-  return (
-    <div>
-      <h2>Edit My Page</h2>
-      <div>
-        <label>ID:</label>
-        <span>{userInfo && userInfo.id}</span>
-      </div>
-      <div>
-        <label>Username:</label>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <button onClick={handleSave}>Save</button>
-      <button onClick={handleCancel}>Cancel</button>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Edit My Page</h2>
+            <div>
+                <label>ID:</label>
+                <span>{userInfo && userInfo.id}</span>
+            </div>
+            <div>
+                <label>Username:</label>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+            <div>
+                <label>Email:</label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <button onClick={handleSave}>Save</button>
+            <button onClick={handleCancel}>Cancel</button>
+        </div>
+    );
 };
 
 export default EditPage;
--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,17 +1,14 @@
 import axios from "axios";
 
 function Error404() {
-
-
     return (
         <>
-          <main class="flex-shrink-0">
-  <div class="container">
-    <h1 class="mt-5">Sticky footer</h1>
-    <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
-    <p>Use <a href="/docs/5.3/examples/sticky-footer-navbar/">the sticky footer with a fixed navbar</a> if need be, too.</p>
-  </div>
-</main>
+            <main className="flex-shrink-0">
+                <div className="container">
+                    <h1 className="mt-5">404 Error</h1>
+                    <p className="lead">PAGE NOT FOUND</p>
+                </div>
+            </main>
         </>
     );
 }
