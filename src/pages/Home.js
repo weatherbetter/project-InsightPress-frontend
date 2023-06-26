777787fa755916708a5c0a36f9380e38142b70c9
weatherbetter12@gmail.com <weatherbetter12@gmail.com>
2023-06-26 16:28:51 +0900

    refactor: 코드 정리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,13 +10,13 @@ function Home() {
     });
 
     useEffect(() => {
-        // axios
-        //     .get(`${process.env.REACT_APP_BOARD_API_URL}/articles`)
-        //     .then((response) => {
-        //         console.log(JSON.parse(response.data.body));
-        //         setArticles(JSON.parse(response.data.body));
-        //     })
-        //     .catch((error) => {});
+        axios
+            .get(`${process.env.REACT_APP_BOARD_API_URL}/articles`)
+            .then((response) => {
+                console.log(JSON.parse(response.data.body));
+                setArticles(JSON.parse(response.data.body));
+            })
+            .catch((error) => {});
     }, []);
 
     return (
@@ -34,44 +34,49 @@ function Home() {
 
                     <div className="row">
                         <div className="col-md-12">
-                            {Array.isArray(articles) && articles.map((article, index) => (
-                                <div
-                                    className="post-entry-1 border-bottom"
-                                    key={index}
-                                >
-                                    <div className="post-meta">
-                                        <span className="date">
-                                            {article_category[article.category]}
-                                        </span>
-                                        <span className="mx-1">•</span>
-                                        <span>
-                                            {article.created_at_article}
-                                        </span>{" "}
-                                        {article.request_id ? (
-                                            <span className="badge text-bg-warning">
-                                                User
-                                            </span>
-                                        ) : (
-                                            <span className="badge text-bg-primary">
-                                                InsightPress
-                                            </span>
-                                        )}
-                                    </div>
-                                    <h2 className="mb-2">
-                                        <Link to={`/article/${article.id}`}>
-                                            {article.title}
-                                        </Link>
-                                    </h2>
-                                    <a
-                                        href={article.source_url}
-                                        className="author mb-3 d-block small"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
+                            {Array.isArray(articles) &&
+                                articles.map((article, index) => (
+                                    <div
+                                        className="post-entry-1 border-bottom"
+                                        key={index}
                                     >
-                                        original article
-                                    </a>
-                                </div>
-                            ))}
+                                        <div className="post-meta">
+                                            <span className="date">
+                                                {
+                                                    article_category[
+                                                        article.category
+                                                    ]
+                                                }
+                                            </span>
+                                            <span className="mx-1">•</span>
+                                            <span>
+                                                {article.created_at_article}
+                                            </span>{" "}
+                                            {article.request_id ? (
+                                                <span className="badge text-bg-warning">
+                                                    User
+                                                </span>
+                                            ) : (
+                                                <span className="badge text-bg-primary">
+                                                    InsightPress
+                                                </span>
+                                            )}
+                                        </div>
+                                        <h2 className="mb-2">
+                                            <Link to={`/article/${article.id}`}>
+                                                {article.title}
+                                            </Link>
+                                        </h2>
+                                        <a
+                                            href={article.source_url}
+                                            className="author mb-3 d-block small"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            original article
+                                        </a>
+                                    </div>
+                                ))}
                         </div>
                     </div>
                 </div>
