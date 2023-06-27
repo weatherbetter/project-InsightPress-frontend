0b278d4c89f8d9125c0d3d9745e4ebeb46f15635
weatherbetter12@gmail.com <weatherbetter12@gmail.com>
2023-06-27 21:59:38 +0900

    style: 로그인 페이지 화면

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,92 +8,116 @@ import { setUserId } from "../store.js";
 
 const Login = () => {
     let dispatch = useDispatch();
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-  const handleChangeId = (e) => setId(e.target.value);
-  const handleChangePassword = (e) => setPassword(e.target.value);
+    const handleChangeId = (e) => setId(e.target.value);
+    const handleChangePassword = (e) => setPassword(e.target.value);
 
-  const handleLogin = (e) => {
-    e.preventDefault(); // Prevent form submission
-    const data = {
-      "httpMethod": "POST",
-      "body": {
-        userid: id,
-        password: password,
-     }
+    const handleLogin = (e) => {
+        e.preventDefault(); // Prevent form submission
+        const data = {
+            httpMethod: "POST",
+            body: {
+                userid: id,
+                password: password,
+            },
+        };
+
+        axios
+            .post(`${process.env.REACT_APP_BOARD_API_URL}/auth/login`, data)
+            .then((res) => {
+                console.log(res);
+                if (res.data.statusCode === 200) {
+                    sessionStorage.setItem("JWT_TOKEN", res.data.body);
+                    sessionStorage.setItem("user_id", id);
+                    //   console.log(id);
+                    //   dispatch(setUserId(id));
+                    alert("Welcome to InsightPress!");
+                    navigate("/");
+                    // redux 로 유저 id 저장!
+                } else {
+                    sessionStorage.clear();
+                    alert(
+                        "Oops, Your account does not exist or wrong password"
+                    );
+                }
+            })
+            .catch((err) => console.log(err));
     };
 
-    axios
-      .post(`${process.env.REACT_APP_BOARD_API_URL}/auth/login`, data)
-      .then((res) => {
-        console.log(res);
-        if (res.data.statusCode === 200) {
-          sessionStorage.setItem("JWT_TOKEN", res.data.body);
-          sessionStorage.setItem("user_id", id);
-        //   console.log(id);
-        //   dispatch(setUserId(id));
-          alert('Welcome to InsightPress!');
-          navigate("/");
-          // redux 로 유저 id 저장!
-          
-        } else {
-          sessionStorage.clear();
-          alert('Oops, Your account does not exist or wrong password');
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            handleLogin(e);
         }
-      })
-      .catch((err) => console.log(err));
-  };
+    };
 
-  const handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      handleLogin(e);
-    }
-  };
+    const handleLogout = () => {
+        sessionStorage.clear(); // .removeItem("JWT_TOKEN"); // 토큰 삭제
 
-  const handleLogout = () => {
-    sessionStorage.clear(); // .removeItem("JWT_TOKEN"); // 토큰 삭제
+        alert("Logged out successfully.");
+        navigate("/login");
+    };
 
-    alert('Logged out successfully.');
-    navigate('/login');
-    
-  };
-  
-  return (
-      <div
-          style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "100vh",
-          }}
-      >
-          <div>
-              ID: <input type="text" value={id} onChange={handleChangeId} />
-              <br />
-              PW:{" "}
-              <input
-                  type="password"
-                  value={password}
-                  onChange={handleChangePassword}
-                  onKeyPress={handleKeyPress}
-              />
-              <br />
-              <button onClick={handleLogin}>Login</button>
-              <button onClick={handleLogout}>Logout</button>
-          </div>
-          <div id="showInputData"></div>
-          <div style={{ textAlign: "center", padding: "30px" }}>
-              <KakaoLogin /> {/* 카카로 로그인 컴포넌트 추가 */}
-          </div>
+    return (
+        <section id="contact" className="contact mb-5">
+            <div className="container aos-init aos-animate" data-aos="fade-up">
+                <div className="row">
+                    <div className="col-lg-12 text-center mb-5">
+                        <h3 className="page-title">Login</h3>
+                    </div>
+                </div>
+                <div className="form col-sm-6 mx-auto">
+                    <form></form>
 
-          <div style={{ textAlign: "center", padding: "30px" }}>
-                <NaverLogin />
+                    <form
+                        //   onSubmit={updatePost.id ? handlerUpdate : addPost}
+                        className="php-email-form"
+                    >
+                        <div className="row">
+                            <div className="form-group col-md-12">
+                                <input
+                                    className="form-control"
+                                    id="disabledTextInput"
+                                    type="text"
+                                    aria-label="default input example"
+                                    name="user"
+                                    value={id}
+                                    onChange={handleChangeId}
+                                    placeholder="아이디를 입력해주세요"
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                value={password}
+                                className="form-control"
+                                required
+                                onChange={handleChangePassword}
+                                onKeyPress={handleKeyPress}
+                                placeholder="비밀번호를 입력해주세요"
+                            />
+                        </div>
+                        <div className="text-center">
+                            <button type="submit" onClick={handleLogin}>
+                                Login
+                            </button>{" "}
+                        </div>
+                    </form>
+                </div>
+                {/* <!-- End Contact Form --> */}
+                <div style={{ textAlign: "center", padding: "30px" }}>
+                    <KakaoLogin /> {/* 카카로 로그인 컴포넌트 추가 */}
+                </div>
+                <div style={{ textAlign: "center", padding: "30px" }}>
+                    <NaverLogin />
+                </div>
             </div>
-      </div>
-  );
+        </section>
+    );
 };
 
 export default Login;
