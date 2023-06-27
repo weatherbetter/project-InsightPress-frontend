eb3dd20fa58c4c101c3e4c66b31cf452ce5838b5
Forestree <classicheroesonamsterdam@gmail.com>
2023-06-27 12:07:36 +0900

    fix: 로그인 기능 구현 (로그아웃 api 삭제 , jwt 토큰 삭제)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import jwt_decode from 'jwt-decode';
 import KakaoLogin from "./KakaoLogin";
 
 const Login = () => {
@@ -38,22 +37,6 @@ const Login = () => {
       .catch((err) => console.log(err));
   };
 
-  // const handleLogout = () => {
-  //   axios
-  //     .post(`${process.env.REACT_APP_BOARD_API_URL}/auth/logout`)
-  //     .then((res) => {
-  //       console.log(res);
-  //       if (res.data.statusCode === 200) {
-  //         sessionStorage.removeItem("JWT_TOKEN"); // 토큰 삭제
-  //         navigate('/login'); // 로그인 페이지로 이동
-  //         alert('Logged out successfully.');
-  //       } else {
-  //         alert('Failed to log out.');
-  //       }
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
-
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       handleLogin(e);
@@ -66,28 +49,6 @@ const Login = () => {
     alert('Logged out successfully.');
     navigate('/login');
     
-    // axios
-    //   .post(`${process.env.REACT_APP_BOARD_API_URL}/auth/logout`)
-    //   .then((res) => {
-    //     console.log(res);
-    //     if (res.data.statusCode === 200) {
-    //       sessionStorage.removeItem("JWT_TOKEN"); // 토큰 삭제
-    //       axios.post(`${process.env.REACT_APP_LAMBDA_API_URL}/logout`) // 로그아웃 람다 함수 호출
-    //         .then((res) => {
-    //           console.log(res);
-    //           if (res.data.statusCode === 200) {
-    //             navigate('/login'); // 로그인 페이지로 이동
-    //             alert('Logged out successfully.');
-    //           } else {
-    //             alert('Failed to log out.');
-    //           }
-    //         })
-    //         .catch((err) => console.log(err));
-    //     } else {
-    //       alert('Failed to log out.');
-    //     }
-    //   })
-    //   .catch((err) => console.log(err));
   };
   
   return (
@@ -121,34 +82,5 @@ const Login = () => {
       </div>
   );
 };
-
-const TokenExpirationCheck = () => {
-  const [isTokenExpired, setIsTokenExpired] = useState(false);
-
-  useEffect(() => {
-    const checkTokenExpiration = () => {
-      const token = sessionStorage.getItem('JWT_TOKEN');
-      if (token) {
-        const decodedToken = jwt_decode(token);
-        const currentTime = Math.floor(Date.now() / 1000);
-        if (decodedToken.exp < currentTime) {
-          setIsTokenExpired(true);
-          sessionStorage.removeItem('JWT_TOKEN');
-          // 추가적인 로그아웃 처리 또는 리다이렉션을 수행할 수 있습니다.
-        }
-      }
-    };
-
-    const intervalId = setInterval(checkTokenExpiration, 60000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  return <div>{isTokenExpired ? 'Token has expired' : 'Token is valid'}</div>;
-};
-
-// export { Login, TokenExpirationCheck };
 
 export default Login;
